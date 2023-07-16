4b54cb28f1489bf8f736b138ba91893f0ef9174f
Adedamola Jude Ajewole <adedamola.ajewole@toptal.com>
2023-07-16 17:52:37 +0200

    create a match with default zeros - passes

---
+++ src/main/java/com/adedamola/scoreboard/Match.java
@@ -0,0 +1,16 @@
+package com.adedamola.scoreboard;
+
+public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private int homeScore;
+    private int awayScore;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+    }
+
+}
--- src/test/java/com/adedamola/scoreboard/MatchTest.java
+++ src/test/java/com/adedamola/scoreboard/MatchTest.java
@@ -9,13 +9,10 @@ public class MatchTest {
     String homeTeam = "Mexico";
     String awayTeam = "Canada";
 
-
     @Test
     public void testCreateAMatchWithDefaultZeroScores(){
         Match match = new Match(homeTeam, awayTeam);
         assertNotNull(match);
     }
-
-
 
 }

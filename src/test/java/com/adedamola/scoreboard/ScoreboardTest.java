c318cc8771be8257d3a138c5f8f0a55d9b11ca88
Adedamola Jude Ajewole <adedamola.ajewole@toptal.com>
2023-07-17 14:12:12 +0200

    Add failing test for checking if match started is in list of matches

--- src/test/java/com/adedamola/scoreboard/ScoreboardTest.java
+++ src/test/java/com/adedamola/scoreboard/ScoreboardTest.java
@@ -2,6 +2,10 @@ package com.adedamola.scoreboard;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class ScoreboardTest {
 
     @Test
@@ -12,4 +16,18 @@ public class ScoreboardTest {
 
         scoreboardManager.startMatch(homeTeam, awayTeam);
     }
+
+    @Test
+    public void testStartMatchAndCheckMatchesStarted() {
+        ScoreboardManager scoreboardManager = new Scoreboard();
+        String homeTeam = "Mexico";
+        String awayTeam = "Canada";
+
+        scoreboardManager.startMatch(homeTeam, awayTeam);
+        List<Match> matches = scoreboardManager.getMatchesSummary();
+        assertEquals(1, matches.size());
+    }
+
+
+
 }

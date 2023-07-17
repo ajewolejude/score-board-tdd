ac42a9aad5fa1505ba6aaf7b5bfe81fe1013469a
Adedamola Jude Ajewole <adedamola.ajewole@toptal.com>
2023-07-17 14:04:52 +0200

    Add initial implementation of startMatch method in Scoreboard class implementing ScoreboardManager interface

---
+++ src/main/java/com/adedamola/scoreboard/Scoreboard.java
@@ -0,0 +1,8 @@
+package com.adedamola.scoreboard;
+
+public class Scoreboard implements ScoreboardManager {
+    @Override
+    public void startMatch(String homeTeam, String awayTeam) {
+
+    }
+}
